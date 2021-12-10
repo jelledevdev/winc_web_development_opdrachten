b3d2580694a104a448e74c17979dfe9d171a6367
jelledevdev <79093022+jelledevdev@users.noreply.github.com>
2021-12-10 15:49:11 +0100

    opdracht 10 eerste js

---
+++ opdracht10_js/index.js
@@ -0,0 +1,14 @@
+
+console.log('dit is mooi');
+let naam = "jelle";
+let leeftijd = 41;
+let leeftijdAlsString = '41'
+console.log(naam);
+console.log(typeof(naam));
+console.log(leeftijd);
+console.log(typeof(leeftijd));
+console.log(leeftijdAlsString);
+console.log(typeof(leeftijdAlsString));
+
+
+
