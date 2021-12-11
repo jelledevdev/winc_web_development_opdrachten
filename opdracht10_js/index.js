bdee90f0e7dc6f0eba8594a70b614e7f6b7d555a
jelledevdev <79093022+jelledevdev@users.noreply.github.com>
2021-12-11 16:00:52 +0100

    oeps files terugzetten

--- opdracht10_js/index.js
+++ opdracht10_js/index.js
@@ -1,14 +1,38 @@
 
 console.log('dit is mooi');
-let naam = "jelle";
+let naam = "jo";
 let leeftijd = 41;
 let leeftijdAlsString = '41'
 console.log(naam);
-console.log(typeof(naam));
+console.log(typeof (naam));
 console.log(leeftijd);
-console.log(typeof(leeftijd));
+console.log(typeof (leeftijd));
 console.log(leeftijdAlsString);
-console.log(typeof(leeftijdAlsString));
+console.log(typeof (leeftijdAlsString));
 
+// This is a big sum
 
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
 
+/* I am a block comment
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
+*/
+
+// Code that is not "on" will not be seen in the console.
+
+// Note: Does JavaScript run in Node? Then your terminal is the console.
+// Does JavaScript run in your browser? Then you will find the console in your developer tools.
+
+if (naam == "jelle") {
+    console.log(naam);
+    document.write("hoi jelle");
+}
+else if (naam == "johan") {
+    console.log(naam);
+    document.write("hoi johan");
+}
+else {
+    document.write("whatever, dan toch lekker niet");
+}
