c589da32a975993e1fd8fdad6af7c4b69b92089a
jelledevdev <79093022+jelledevdev@users.noreply.github.com>
2021-12-11 15:58:03 +0100

    opdracht 11 if else js

---
+++ opdracht11_js/index.js
@@ -0,0 +1,25 @@
+const age = 17;
+
+if (age > 17){
+    document.write("kom binnen!  ")
+}
+else{
+    document.write("Sorry maar jij bent nog te jong om hier naar binnen te gaan.  ")
+}
+document.write("<br>")
+const isFemale = true
+
+if (isFemale){
+    document.write(" je mag een ticket kopen, want je bent een vrouw.  ")
+}
+else {document.write("sorry, maar je bent geen man, dus toedeledokie!  ")}
+document.write("<br>")
+const driverStatus = "bob"
+
+if (driverStatus == "bob") {
+    document.write("je mag naar huis rijden.  ")
+}
+else {
+    document.write("je bent dronken. Hier met die autosleutels.  ")
+    
+}
