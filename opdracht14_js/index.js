9fe632add9965862943df374f2d773b7efc87b72
jelledevdev <79093022+jelledevdev@users.noreply.github.com>
2021-12-13 11:55:08 +0100

    opdracht 14 function

---
+++ opdracht14_js/index.js
@@ -0,0 +1,27 @@
+console.log("Defining a function for making a pizza")
+
+const makePizza = function (){
+    console.log("prepare the dough")
+    console.log("knead it and shape it pizza-style")
+    console.log("apply tomato sauce")
+    console.log("add oregano, grated cheese and mozzarella")
+    console.log("add your favorite toppings")
+    console.log("bake the pizza approx. 8 minutes in your oven")
+    console.log("share and eat!")
+}
+
+const makeSushi = function (){
+    console.log("boil sushi rice")
+    console.log("add vinegar")
+    console.log("slice avocado and tuna")
+    console.log("take 1 nori sheet, and add the ingredients")
+    console.log("roll it!")
+    console.log("share and eat!")
+}
+
+makeSushi()
+makeSushi()
+makeSushi()
+
+makePizza()
+makePizza()
